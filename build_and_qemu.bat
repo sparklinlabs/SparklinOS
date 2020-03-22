b718c57b48f2712b59226e191f8a032390cf13d5
Elisée Maurer <elisee@sparklinlabs.com>
2020-03-23 00:39:39 +0100

    Fix warning when running in QEMU

--- build_and_qemu.bat
+++ build_and_qemu.bat
@@ -1,5 +1,5 @@
 @call build.bat
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
-"C:\Program Files\qemu\qemu-system-x86_64.exe" bin\disk.bin
+"C:\Program Files\qemu\qemu-system-x86_64.exe" -drive format=raw,file=bin\disk.bin
 @if %errorlevel% neq 0 exit /b %errorlevel%
